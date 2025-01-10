24132202afca9f76777de8d64fb16b51bbe37875
SakonLight90 <66551324+SakonLight90@users.noreply.github.com>
2025-01-10 03:29:26 +0100

    Update savage.js

--- savage.js
+++ savage.js
@@ -1,5 +1,8 @@
 function changeContent(contentType) {
     let videoId = '';
+    const contentContainer = document.getElementById('contentContainer');
+    contentContainer.innerHTML = '<div class="loading">Caricamento...</div>';  // Mostra il messaggio di caricamento
+
     if (contentType === 'youtubeLive' || contentType === 'youtubeChat') {
         fetch('http://live.ultimateita.it/videoid.txt')
             .then(response => response.text())
@@ -9,7 +12,7 @@ function changeContent(contentType) {
             })
             .catch(error => {
                 console.error('Errore nel recupero del file videoid.txt:', error);
-                document.getElementById('contentContainer').innerHTML = '<p>Errore nel caricare il contenuto.</p>';
+                loadEmbed('error');  // Carica il messaggio di errore
             });
     } else {
         loadEmbed(contentType);
@@ -17,6 +20,7 @@ function changeContent(contentType) {
 }
 
 function loadEmbed(contentType, videoId = '') {
+    const contentContainer = document.getElementById('contentContainer');
     let embedCode = '';
     switch(contentType) {
         case 'twitch':
@@ -40,10 +44,14 @@ function loadEmbed(contentType, videoId = '') {
         case 'discord':
             embedCode = `<iframe src="https://discord.com/widget?id=YOUR_SERVER_ID&theme=light" width="100%" height="600" frameborder="0" allowtransparency="true"></iframe>`;
             break;
+        case 'error':
+            embedCode = `<div class="error-message">Errore nel caricamento del contenuto. Riprova più tardi.</div>`;
+            break;
         default:
-            embedCode = '<p>Contenuto non trovato.</p>';
+            embedCode = `<div class="error-message">Contenuto non trovato.</div>`;
+            break;
     }
-    document.getElementById('contentContainer').innerHTML = embedCode;
+    contentContainer.innerHTML = embedCode;
 }
 
 function toggleTheme() {
